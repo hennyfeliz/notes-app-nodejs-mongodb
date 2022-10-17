5bdc4bed7ba42ff627977f0b9a59deffb58d9098
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2022-10-16 20:10:42 -0400

    notes design

--- routes/notes.js
+++ routes/notes.js
@@ -1,22 +1,31 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 
 /* routes */
 
 /* GET */
-router.get('/notes/add', (req, res) => {
-  res.render('notes/new-note')
-})
+router.get("/notes/add", (req, res) => {
+  res.render("notes/new-note");
+});
 
 /* POST */
-router.post('/notes/new-note', (req, res) => {
-  console.log(req.body)
-  res.send('ok')
-})
-
+router.post("/notes/new-note", (req, res) => {
+  const { title, description } = req.body;
+  const errors = [];
+  if (!title) errors.push({ text: "please, insert a title!" });
+  if (!description) errors.push({ text: "please, insert a description" });
+  if (errors.length > 0) {
+    res.render("notes/new-note", {
+      errors,
+      title,
+      description,
+    });
+  } else {
+    res.send("ok");
+  }
+});
 
 /* PUT */
 
-
 /* DELETE */
-export default router
+export default router;
