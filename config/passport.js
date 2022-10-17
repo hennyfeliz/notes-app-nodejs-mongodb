8c053fab727989f067300da3c73b66eb1f9e07a0
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2022-10-17 06:32:49 -0400

    adding passport config

---
+++ config/passport.js
@@ -0,0 +1,38 @@
+import passport from "passport";
+import { Strategy } from "passport-local";
+import User from "../models/User";
+
+passport.use(
+  new Strategy(
+    {
+      usernameField: "email",
+    },
+    async (email, password, done) => {
+      const user = User.findOne({ email: email });
+      if (!user) {
+        return done(null, false, {
+          message: "not found user...",
+        });
+      } else {
+        const match = await User.matchPassword(password);
+        if (match) {
+          return done(null, user);
+        } else {
+          return done(null, false, {
+            message: "incorrect password...",
+          });
+        }
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user)
+  })
+})
